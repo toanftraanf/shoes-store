4fb7b3ed4e1a4c6ea948499b59f8d99f960a4f02
toantt <tranthetoan2003@gmail.com>
2023-12-05 01:52:15 +0700

    Add homepage, dashboard, user list page

---
+++ ShoesStore/ShoesStore/Models/OrderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoesStore.Models
+{
+    public partial class OrderDetail
+    {
+        public int DetailsId { get; set; }
+        public int? ProductId { get; set; }
+        public decimal? Price { get; set; }
+        public int? Quantity { get; set; }
+        public int? OrderId { get; set; }
+
+        public virtual Order? Order { get; set; }
+        public virtual Product? Product { get; set; }
+    }
+}
---
+++ ShoesStore/ShoesStore/Models/ShoesStoreContext.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ShoesStore.Models
+{
+    public partial class ShoesStoreContext : DbContext
+    {
+        public ShoesStoreContext()
+        {
+        }
+
+        public ShoesStoreContext(DbContextOptions<ShoesStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Brand> Brands { get; set; } = null!;
+        public virtual DbSet<Color> Colors { get; set; } = null!;
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+        public virtual DbSet<ProductCategory> ProductCategories { get; set; } = null!;
+        public virtual DbSet<ProductDetail> ProductDetails { get; set; } = null!;
+        public virtual DbSet<Size> Sizes { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder()
+                              .SetBasePath(Directory.GetCurrentDirectory())
+                              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyCnn"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Brand>(entity =>
+            {
+                entity.ToTable("brand");
+
+                entity.Property(e => e.BrandId).HasColumnName("brand_id");
+
+                entity.Property(e => e.DisplayOrder).HasColumnName("display_order");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(255)
+                    .HasColumnName("name");
+            });
+
+            modelBuilder.Entity<Color>(entity =>
+            {
+                entity.ToTable("color");
+
+                entity.Property(e => e.ColorId).HasColumnName("color_id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .HasColumnName("name");
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.ToTable("order");
+
+                entity.Property(e => e.OrderId).HasColumnName("order_id");
+
+                entity.Property(e => e.CustomerId).HasColumnName("customer_id");
+
+                entity.Property(e => e.Discount).HasColumnName("discount");
+
+                entity.Property(e => e.OrderDate)
+                    .HasColumnType("datetime")
+                    .HasColumnName("order_date");
+
+                entity.Property(e => e.Status).HasColumnName("status");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.CustomerId)
+                    .HasConstraintName("FK_order_user");
+            });
+
+            modelBuilder.Entity<OrderDetail>(entity =>
+            {
+                entity.HasKey(e => e.DetailsId);
+
+                entity.ToTable("order_details");
+
+                entity.Property(e => e.DetailsId).HasColumnName("details_id");
+
+                entity.Property(e => e.OrderId).HasColumnName("order_id");
+
+                entity.Property(e => e.Price)
+                    .HasColumnType("decimal(18, 0)")
+                    .HasColumnName("price");
+
+                entity.Property(e => e.ProductId).HasColumnName("product_id");
+
+                entity.Property(e => e.Quantity).HasColumnName("quantity");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.OrderId)
+                    .HasConstraintName("FK_order_details_order");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK_order_details_product");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("product");
+
+                entity.Property(e => e.ProductId).HasColumnName("product_id");
+
+                entity.Property(e => e.BrandId).HasColumnName("brand_id");
+
+                entity.Property(e => e.CateId).HasColumnName("cate_id");
+
+                entity.Property(e => e.Description)
+                    .HasColumnType("ntext")
+                    .HasColumnName("description");
+
+                entity.Property(e => e.DisplayOrder).HasColumnName("display_order");
+
+                entity.Property(e => e.Image)
+                    .HasMaxLength(255)
+                    .HasColumnName("image");
+
+                entity.Property(e => e.IsHot)
+                    .HasColumnType("datetime")
+                    .HasColumnName("is_hot");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(255)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Price)
+                    .HasColumnType("decimal(18, 0)")
+                    .HasColumnName("price");
+
+                entity.Property(e => e.PromotionPrice)
+                    .HasColumnType("decimal(18, 0)")
+                    .HasColumnName("promotion_price");
+
+                entity.Property(e => e.Quantity).HasColumnName("quantity");
+
+                entity.Property(e => e.Status).HasColumnName("status");
+
+                entity.Property(e => e.Vat).HasColumnName("vat");
+
+                entity.Property(e => e.Warranty).HasColumnName("warranty");
+
+                entity.HasOne(d => d.Cate)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CateId)
+                    .HasConstraintName("FK_product_brand");
+
+                entity.HasOne(d => d.CateNavigation)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CateId)
+                    .HasConstraintName("FK_product_product_category");
+            });
+
+            modelBuilder.Entity<ProductCategory>(entity =>
+            {
+                entity.HasKey(e => e.CateId);
+
+                entity.ToTable("product_category");
+
+                entity.Property(e => e.CateId).HasColumnName("cate_id");
+
+                entity.Property(e => e.DisplayOrder).HasColumnName("display_order");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.ParentId).HasColumnName("parent_id");
+
+                entity.Property(e => e.Status).HasColumnName("status");
+            });
+
+            modelBuilder.Entity<ProductDetail>(entity =>
+            {
+                entity.HasKey(e => e.DetailsId);
+
+                entity.ToTable("product_details");
+
+                entity.Property(e => e.DetailsId).HasColumnName("details_id");
+
+                entity.Property(e => e.ColorId).HasColumnName("color_id");
+
+                entity.Property(e => e.ProductId).HasColumnName("product_id");
+
+                entity.Property(e => e.Quantity).HasColumnName("quantity");
+
+                entity.Property(e => e.SizeId).HasColumnName("size_id");
+
+                entity.HasOne(d => d.Color)
+                    .WithMany(p => p.ProductDetails)
+                    .HasForeignKey(d => d.ColorId)
+                    .HasConstraintName("FK_product_details_color");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.ProductDetails)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK_product_details_product");
+
+                entity.HasOne(d => d.Size)
+                    .WithMany(p => p.ProductDetails)
+                    .HasForeignKey(d => d.SizeId)
+                    .HasConstraintName("FK_product_details_size");
+            });
+
+            modelBuilder.Entity<Size>(entity =>
+            {
+                entity.ToTable("size");
+
+                entity.Property(e => e.SizeId).HasColumnName("size_id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(10)
+                    .HasColumnName("name");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("user");
+
+                entity.Property(e => e.UserId).HasColumnName("user_id");
+
+                entity.Property(e => e.Address)
+                    .HasColumnType("ntext")
+                    .HasColumnName("address");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Mobile)
+                    .HasMaxLength(11)
+                    .HasColumnName("mobile");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(21)
+                    .HasColumnName("password");
+
+                entity.Property(e => e.Role).HasColumnName("role");
+
+                entity.Property(e => e.Status).HasColumnName("status");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
